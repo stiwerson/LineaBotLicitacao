bd80181c7e2aad507c743efc49c61b0432309f09
stiwerson <stiwerson@gmail.com>
2023-03-14 16:13:07 -0300

    Finally clicking on the button i'm not a robot, downloaded the extension buster to solve the annoying captcha

--- scripts/startSelenium.js
+++ scripts/startSelenium.js
@@ -1,35 +1,39 @@
 const colors = require('colors');
 const {writeErrorLog} = require('./errorLogger');
-const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+const {Builder} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const path = require('path');
 
-const chromiumDriverPath = path.join(__dirname, '/chromium-web-driver/chromedriver')
+const chromiumDriverPath = path.join(__dirname, '../chromium-web-driver/chromedriver')
 
 module.exports.startSelenium = async (url,city) => {
     console.log("Iniciando o selenium");
 
     const options = new chrome.Options();
+    options.addArguments('--no-sandbox', '--disable-dev-shm-usage', '--remote-debugging-port=9222');
+    options.setChromeBinaryPath('')
 
     //If you gonna debug comment this code below, so you can fully preview selenium working
     //options.headless()
-
+    let driver
     try{
-        const driver = await new Builder()
+         driver = await new Builder()
             .forBrowser('chrome')
             .setChromeService(new chrome.ServiceBuilder(chromiumDriverPath))
             .setChromeOptions(options)
             .build();
     }catch(e){
         console.log(`Não foi possivel iniciar o selenium`.red)
-        writeErrorLog(`Não foi possivel iniciar o selenium`);
+        writeErrorLog(`Não foi possivel iniciar o selenium\nError Log: ${e}`);
     }
 
     console.log('Indo ao site de ' + city);
     try{
         await driver.get(url);
+        
     }catch(e){
         console.log(`Não foi possivel ir ate o site de ${city}`.red);
         writeErrorLog(`${city}: Não foi possível acessar o site.\nVerifique se o site ${url} esta fora do ar.\nError Log:\n${e}`)
     }
+    return {driver};
 }
--- scripts/template2.js
+++ scripts/template2.js
@@ -1,9 +1,34 @@
 const colors = require('colors');
 const {writeErrorLog} = require('./errorLogger');
 const {gerarEdital, saveBiddings} = require('./functions');
-const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+const {Builder, Browser, By, Key, until, WebDriver} = require('selenium-webdriver');
+const {startSelenium} = require('./startSelenium');
 const chrome = require('selenium-webdriver/chrome');
 
 module.exports.getTemplate2Biddings = async (url, city) =>{
+    const {driver} = await startSelenium(url,city);
 
+    //Solving Captcha if exists
+    let captchaFrame;
+    try{
+        console.log('Checando se tem algum recaptcha');
+
+        //Get the iframe of the captcha
+        captchaFrame = await driver.wait(until.elementsLocated(By.css('iframe[title="reCAPTCHA"]')), 10000);
+
+        console.log('Indo para o iframe do captcha');
+        await driver.switchTo().frame(captchaFrame[1]);
+
+        //I'm not a robot checkbox
+        console.log('Localizando o botão (Não sou um robô)');
+        const checkbox = await driver.findElement(By.css('.recaptcha-checkbox-border'));
+        
+        //Click checkbox
+        console.log("Clicando no botão (Não sou um robô)");
+        await driver.actions().click(checkbox).perform();
+
+    }catch(e){
+        console.log("RECAPTCHA não encontrado.\nError Log: "+e)
+        driver.close();
+    }
 }
